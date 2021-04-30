ceefc8fda0060508bde734b5e488ff2904b5b51c
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2021-04-30 13:20:56 -0700

    yo yo yo yo yo...

--- src/FNFCharter++.cpp
+++ src/FNFCharter++.cpp
@@ -1,24 +1,52 @@
-#include <windows.h> //legit just to hide the console window
-#include <thread>
+//includes
+//to hide console window
+#include <windows.h>
+//its vector yay
+#include <vector>
+//i shouldnt need to explain this, its iostream
+#include <iostream>
+//t i m e
 #include <chrono>
-#include "lib/nana/gui.hpp" //gui
-#include "lib/nana/gui/widgets/label.hpp" //gui
-void delay_move(HWND hWnd) {
-    RECT desktop;
-    GetClientRect(GetDesktopWindow(), &desktop);
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    MoveWindow(hWnd, (desktop.right/2) - 640, (desktop.bottom/2) - 360, 1280, 720, true);
-}
+//gui crap (nana libs)
+#include "lib/nana/gui.hpp"
+#include "lib/nana/gui/widgets/label.hpp"
+//its the main one
 int main() {
-    ShowWindow(GetConsoleWindow(), SW_HIDE); //hide console window bc its annoying
-    nana::form fm;
-    fm.caption("FNFCharter++");
-    nana::label lb{fm, nana::rectangle{10, 10, 100, 100}};
-    lb.caption("hi b*tches");
+    //hide console window and make window with 320x512 size
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+    nana::form fm(nana::API::make_center(320, 512));
+    //make vector for grid
+    std::vector<std::unique_ptr<nana::label>> lbs;
+    //some empty labels but they need to be different for some reason
+    nana::label voidlb(fm);
+    nana::label voidlb2(fm);
+    //set bg colors
+    voidlb.bgcolor(nana::colors::blue);
+    voidlb2.bgcolor(nana::colors::blue);
+    //make the layout
+    nana::place layout(fm);
+    //so, 64 width area, then 128 width evenly spaces 4x16 grid, everything else is the last section
+    layout.div("<voidd weight=64><notearea grid=[4, 16] weight=128><void2electricboogaloo>");
+    //now we fill that grid with labels using our vector
+    for (int i = 0; i < 64; i++) {
+        //first create a temp pointer to nana::label object placed on fm
+        lbs.emplace_back(new nana::label(fm));
+        //set caption and bg color
+        lbs.back()->caption("yo");
+        lbs.back()->bgcolor(nana::colors::dark_blue);
+        //add to the grid
+        layout.field("notearea") << *lbs.back();
+    }
+    //dont need whats in this vector anymore but we cant clear it or else all of our labels dissapear
+    //add our blank labels to layout
+    layout["voidd"] << voidlb;
+    layout["void2electricboogaloo"] << voidlb2;
+    //"compile" layout
+    layout.collocate();
+    //show form
     fm.show();
-    HWND hWnd = FindWindowA(0, "FNFCharter++");
-    std::thread resize (delay_move, hWnd);
+    //run our program (actually startes event handling but whatever) this ends when our form closes
     nana::exec();
-    resize.join();
+    //we got through it ok so return 0
     return 0;
 }
